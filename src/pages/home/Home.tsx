b22f3f6f7a88f8dc3f229f35ddb5c60894f2eed9
junk-sound <wom2277@naver.com>
2021-05-28 23:45:31 +0900

    control width height of logo

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import TopNavigationSection from "src/pages/home/sections/TopNavigation";
 import IntroductionSection from "src/pages/home/sections/Introduction";
-import logo from './logo.svg';
 
 const HomePage: React.FC = () => {
 
 
   return <div>
-    <img src={logo} className="App-logo" alt="logo" />
     <TopNavigationSection />
     <IntroductionSection />
 
--- src/pages/home/sections/TopNavigation.tsx
+++ src/pages/home/sections/TopNavigation.tsx
@@ -3,6 +3,8 @@ import {css} from "@emotion/react";
 import Colors from "src/pages/constants/Colors";
 import Sizes from "src/pages/constants/Sizes";
 import TopNavigationBar from "src/pages/components/bars/navigation/Top";
+import {ReactComponent as Logo} from "src/pages/home/logo.svg";
+import Pixel from "src/graphic/size/pixel";
 
 const TopNavigationSection: React.FC = () => {
   const topNavigationContainerStyle = css({
@@ -10,6 +12,7 @@ const TopNavigationSection: React.FC = () => {
     height: Sizes.layout.bar.top.value
   });
   return <div>
+    <Logo width={new Pixel(100).value} height={new Pixel(100).value} />
     <TopNavigationBar containerStyle={topNavigationContainerStyle}/>
   </div>;
 };
