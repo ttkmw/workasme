1ccc829acbacad37edcf8b7bfca27d7f6f61b85e
junk-sound <wom2277@naver.com>
2021-05-28 22:19:17 +0900

    desing logo

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TopNavigationSection from "src/pages/home/sections/TopNavigation";
+import IntroductionSection from "src/pages/home/sections/Introduction";
 
 const HomePage: React.FC = () => {
 
 
   return <div>
     <TopNavigationSection />
+    <IntroductionSection />
 
   </div>;
 };
