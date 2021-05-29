356a709024fc0c16655f94296b7f6694d84eac1f
junk-sound <wom2277@naver.com>
2021-05-29 12:06:49 +0900

    move constants

--- src/pages/home/sections/TopNavigation.tsx
+++ src/pages/home/sections/TopNavigation.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import {css} from "@emotion/react";
-import Colors from "src/pages/constants/Colors";
-import Sizes from "src/pages/constants/Sizes";
 import TopNavigationBar from "src/pages/components/bars/navigation/Top";
 import {ReactComponent as Logo} from "src/pages/home/logo.svg";
 import Pixel from "src/graphic/size/pixel";
+import Colors from "src/constants/Colors";
+import Sizes from "src/constants/Sizes";
 
 const TopNavigationSection: React.FC = () => {
   const topNavigationContainerStyle = css({
