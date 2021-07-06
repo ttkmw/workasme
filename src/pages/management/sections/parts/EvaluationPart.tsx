ef4588eacfe1291d399953d8c6f7a99654f82c8f
junk-sound <wom2277@naver.com>
2021-07-06 16:44:10 +0900

    구글차트로 됨

--- src/pages/management/sections/parts/EvaluationPart.tsx
+++ src/pages/management/sections/parts/EvaluationPart.tsx
@@ -3,6 +3,7 @@ import Pixel from "src/graphic/size/pixel";
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import {css, jsx} from "@emotion/react";
+import Chart from "react-google-charts";
 
 const EvaluationPart: React.FC<{height: Pixel}> = (props: {height: Pixel}) => {
   const {height} = props;
@@ -10,9 +11,33 @@ const EvaluationPart: React.FC<{height: Pixel}> = (props: {height: Pixel}) => {
     backgroundColor: 'red',
     height: height.value
   })}>
-
+    <FeedbackComponent />
   </div>
-};;
+};
+
+const FeedbackComponent: React.FC = () => {
+  return <div>hahaha</div>
+};
+
+const EvaluationPieChartComponent: React.FC = () => {
+  return <Chart
+    width={'500px'}
+    height={'300px'}
+    chartType="PieChart"
+    loader={<div>Loading Chart</div>}
+    data={[
+      ['Time Category', 'Hours per Day'],
+      ['Mental', 11],
+      ['Physical', 2],
+      ['Intellectual', 2],
+      ['etc', 9],
+    ]}
+    options={{
+      title: 'Time Track Results',
+    }}
+    rootProps={{ 'data-testid': '1' }}
+  />;
+};
 
 
 const EvaluationGraphPart: React.FC = () => {
