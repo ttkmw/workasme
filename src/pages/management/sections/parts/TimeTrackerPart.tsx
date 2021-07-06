c519428cf1d7cdeb1cfc3e0b161aad3d9e141d50
junk-sound <wom2277@naver.com>
2021-07-06 13:10:16 +0900

    before exctract Cell

--- src/pages/management/sections/parts/TimeTrackerPart.tsx
+++ src/pages/management/sections/parts/TimeTrackerPart.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Pixel from "src/graphic/size/pixel";
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import {css, jsx} from "@emotion/react";
 import Colors from "src/constants/Colors";
+import {TimeTrackerRowDto} from "src/pages/management/sections/parts/dtos/TimeTrackerRowDto";
+import {Table, Toast} from "react-bootstrap";
 
 // todo: props 따로 빼기
 const TimeTrackerPart: React.FC<{ marginVertical: Pixel }> = (props: { marginVertical: Pixel }) => {
   const {marginVertical} = props;
 
+
+  const [rows, setRows] = useState<TimeTrackerRowDto[]>([
+    {
+      expectedTime: "TimTrakerPart",
+      expectedPeriiod: "2 hour",
+      acutualTime: "TimeTrackerPart",
+      actualPeriod: "3 hour"
+    }
+  ]);
 
 
   return <div css={css({
@@ -16,14 +27,24 @@ const TimeTrackerPart: React.FC<{ marginVertical: Pixel }> = (props: { marginVer
     marginBottom: marginVertical.value
   })}>
 
-    <TimeTrackerTable rows={rows} isUpdating={isUpdating}/>
+    <TimeTrackerTable/>
 
   </div>
 };
 
 const TimeTrackerTable: React.FC = () => {
 
-  return <div></div>
+  return <Table bordered hover>
+    <thead>
+    <tr>
+      <th>Expected Time</th>
+      <th>Expected Period</th>
+      <th>Acutal Time</th>
+      <th>Actual Period</th>
+      <th>Time Category</th>
+    </tr>
+    </thead>
+  </Table>
 };
 
 export default TimeTrackerPart;
