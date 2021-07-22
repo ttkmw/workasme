48237ef8977b3170d661f38403a82796df34ed5b
junk-sound <wom2277@naver.com>
2021-07-22 13:57:10 +0900

    주소 변경

--- src/api/adapterFactory/axiosFactory.ts
+++ src/api/adapterFactory/axiosFactory.ts
@@ -4,6 +4,6 @@ const createAxios = (config: AxiosRequestConfig): AxiosInstance => {
   return axios.create(config);
 };
 
-export const host = '3.36.108.240:8081';
+export const host = '13.124.89.189:8081';
 
 export default createAxios;
