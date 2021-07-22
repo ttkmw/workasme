83811654d634698b952a51541a6302862c3195e3
junk-sound <wom2277@naver.com>
2021-07-22 14:25:22 +0900

    proxy 적용해봄

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use('/api',
+    createProxyMiddleware({
+      target: "http://13.124.89.189:8081",
+      changeOrigin: true
+    })
+  )
+};
