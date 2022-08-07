41b64fc05105b27739b658f405044846ec4a2fbc
junk-sound <wom2277@naver.com>
2022-08-07 17:38:23 +0900

    date picker css 했고, timepicker 할 차례

--- src/pages/components/DatePicker.tsx
+++ src/pages/components/DatePicker.tsx
@@ -6,6 +6,7 @@ import * as ReactDatePicker from "react-datepicker";
 import {css, jsx} from "@emotion/react";
 import "react-datepicker/dist/react-datepicker.css";
 import Pixel from "src/graphic/size/pixel";
+import Colors from "src/constants/Colors";
 
 //https://reactdatepicker.com/
 const DatePicker: React.FC = () => {
@@ -22,14 +23,24 @@ const DatePicker: React.FC = () => {
   return (
     <div css={css({
       ".picker-input": {
-        backgroundColor: "orange",
-        width: new Pixel(100).toString()
+        // backgroundColor: "orange",
+        textAlign: "center",
+        width: new Pixel(90).toString(),
+        borderLeftWidth: new Pixel(0).toString(),
+        borderTopWidth: new Pixel(0).toString(),
+        borderRightWidth: new Pixel(0).toString(),
+        borderBottomColor: "#ced4da",
+        borderBottomWidth: new Pixel(1).toString(),
+        ":focus-visible": {
+          outline: "0px"
+        }
+        // border: 0
       },
       ".react-datepicker__day--selected": {
-        backgroundColor: "orange"
+        backgroundColor: Colors.theme.main.work
       },
       ".react-datepicker__day--keyboard-selected": {
-        backgroundColor: "orange"
+        backgroundColor: Colors.theme.main.work
       }
       // ".react-datepicker__day--selecting": {
       //   backgroundColor: "orange"
