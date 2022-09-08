16623718ac06453bc13f488265562192d9efe995
junk-sound <wom2277@naver.com>
2022-09-08 12:45:06 +0900

    redux-persist 적용

---
+++ src/context/rootReducer.ts
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux'
+import signReducer from "src/context/signSlice";
+import timeReducer from "src/context/timeSlice";
+import usernameReducer from "src/context/usernameSlice";
+import passwordReducer from "src/context/passwordSlice";
+
+const rootReducer = combineReducers({
+  sign: signReducer,
+  time: timeReducer,
+  username: usernameReducer,
+  password: passwordReducer,
+})
+
+export default rootReducer;
--- src/index.tsx
+++ src/index.tsx
@@ -7,24 +7,29 @@ import reportWebVitals from "src/reportWebVitals";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TimeTrackersPage from "src/pages/management/TimeTrackersPage";
 import {Provider} from "react-redux";
-import {store} from "src/context/store";
+import {persistor, store} from "src/context/store";
 import SignInPage from "src/pages/SignInPage";
 import SignUpPage from "src/pages/SignUpPage";
 import DeviceDetector from "src/DeviceDetector";
+import {PersistGate} from "redux-persist/integration/react";
 
 //브라우저 캐시 지우는거
 //https://thewebdev.info/2021/12/03/how-to-clear-browser-cache-in-react-2/
 ReactDOM.render(
   <Provider store={store}>
-    <DeviceDetector>
-      <Router>
-        <Routes>
-          <Route path={"/"}  element={<SignInPage/>}/>
-          <Route path={"/time-track"}  element={<TimeTrackersPage />}/>
-          <Route path={"/sign-up"} element={<SignUpPage />} />
-        </Routes>
-      </Router>
-    </DeviceDetector>
+    <PersistGate loading={null} persistor={persistor}>
+      <DeviceDetector>
+        <Router>
+          <Routes>
+            <Route path={"/"}  element={<SignInPage/>}/>
+            <Route path={"/time-track"}  element={<TimeTrackersPage />}/>
+            <Route path={"/sign-up"} element={<SignUpPage />} />
+          </Routes>
+        </Router>
+        
+      </DeviceDetector>
+    </PersistGate>
+
   </Provider>,
   document.getElementById('root')
 );
